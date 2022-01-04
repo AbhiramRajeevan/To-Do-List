71997d165b2b17c11522411ff05c55d1bde4ac01
Abhiram Rajeevan <abhiramrajiv14@gmail.com>
2022-01-04 23:38:48 +0530

    Add delete instruction item to default list

--- app.js
+++ app.js
@@ -30,7 +30,11 @@ const item2 = new Item({
   name: "Hit the + button to add a new item"
 })
 
-const defaultItems = [item1, item2]
+const item3 = new Item({
+  name: "<--Hit this to delete an item"
+})
+
+const defaultItems = [item1, item2,item3]
 
 const listSchema = {
   name : String,
